180702a8a1b409a0b78d53076d8a52668abb688d
rainyrainyguo <1695428348@qq.com>
2019-03-21 05:40:03 +0800

    LeapYear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -2,6 +2,10 @@
  *  @author YOUR NAME HERE
  */
 public class LeapYear {
+    /** tell you whether a year is a leap year */
+	public static boolean isLeapYear(int year){
+        return (year%400==0) || (year%4==0 && year%100!=0);
+	}
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
@@ -18,7 +22,7 @@ public class LeapYear {
     public static void main(String[] args) {
         if (args.length < 1) {
             System.out.println("Please enter command line arguments.");
-            System.out.println("e.g. java Year 2000");
+            System.out.println("e.g. java LeapYear 2000");
         }
         for (int i = 0; i < args.length; i++) {
             try {
