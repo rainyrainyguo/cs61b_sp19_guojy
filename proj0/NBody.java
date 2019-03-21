c7e8a2d643e195f5b58eea305ae30a21a1e96395
rainyrainyguo <1695428348@qq.com>
2019-03-22 01:52:45 +0800

    update NBody.java

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class NBody{
     public static double readRadius(String dir) {
         In in = new In(dir);
@@ -7,21 +10,35 @@ public class NBody{
         return R;
     }
     public static Body[] readBodies(String dir){
-    	Body[] bodyarr = new Body [5];
+    	//Body[] bodyarr = new Body [5];
+    	List<Body> bodyls = new ArrayList<Body>();
+
     	In in = new In(dir);
         int N = in.readInt();
         double R = in.readDouble();
-    	for(int i=0;i<5;i++){
-    		double xxPos = in.readDouble();
-    		double yyPos = in.readDouble();
-    		double xxVel = in.readDouble();
-    		double yyVel = in.readDouble();
-    		double mass = in.readDouble();
-    		String img = in.readString();
-    		bodyarr[i] = new Body(xxPos, yyPos, xxVel, yyVel, mass, img);
-    	}
+
+        while(!in.isEmpty()){
+        	String xxPs = in.readString();
+        	try{
+        		double xxPos = Double.parseDouble(xxPs);
+        	} catch(java.lang.NumberFormatException e){
+        		break;
+        	}
+        	double xxPos = Double.parseDouble(xxPs);
+        	double yyPos = Double.parseDouble(in.readString());
+        	double xxVel = Double.parseDouble(in.readString());
+        	double yyVel = Double.parseDouble(in.readString());
+        	double mass = Double.parseDouble(in.readString());
+        	String img = in.readString();
+        	bodyls.add(new Body(xxPos, yyPos, xxVel, yyVel, mass, img));
+        }
+
+        Body bodyarr[] = new Body[bodyls.size()];
+        bodyls.toArray(bodyarr);
+
     	return bodyarr;
     }
+
     public static void main(String[] args){
 		double T = Double.parseDouble(args[0]);
     	double dt = Double.parseDouble(args[1]);
@@ -53,5 +70,13 @@ public class NBody{
         	StdDraw.show();
         	StdDraw.pause(10);
         }
+
+        StdOut.printf("%d\n", bodies.length);
+		StdOut.printf("%.2e\n", radius);
+		for (int i = 0; i < bodies.length; i++) {
+		    StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+		                  bodies[i].xxPos, bodies[i].yyPos, bodies[i].xxVel,
+		                  bodies[i].yyVel, bodies[i].mass, bodies[i].imgFileName);   
+		}
     }
 }
